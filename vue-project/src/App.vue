11021b44d9b0d758857d58fbce4933f5d4a3ebe3
Kong Vongpisith <115731062+KongVongPisith@users.noreply.github.com>
2023-06-22 16:09:25 +0700

    vue-project

---
+++ vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <HelloWorld/>
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,74 @@
+<template>
+    <body>
+        <header>
+            <h1 class="h1">Blazer-Eco</h1>
+        </header>
+
+        <main class="main">
+            <section class="wrapper-top">
+                <article class="top">
+                    <article>
+                        <h2>Buy & Sell Accessories in one click</h2>
+                        <input class="search" type="text" placeholder="search">
+                    </article>
+                    <article>
+                         <img src="../assets/img/pic1 (6).png" alt="" width="471px" height="343px"/>
+                    </article>
+                   
+                </article>
+            </section>
+
+            <section class="serve">
+                <article class="serve1">
+                    <article class="usedandnew">
+                        <h3>Used & New</h3>
+                        <p>We sell any accessories for daily life and working</p>
+                        <button>View More</button>
+                    </article>
+                    <article>
+                        <img src="../assets/img/pic1 (1).jpg" alt="" width="214px" height="215px">
+                    </article>
+                </article>
+
+                <article class="serve2">
+                    <article>
+                        <h3>Services</h3>
+                        <p>Warranty for 6 month and discount for fixing otherwise if our product is disable you can contact us and we will refund money back</p>
+                        <button>View More</button>
+                    </article>
+                    <article>
+                        <img src="../assets/img/pic1 (7).png" alt="" width="214px" height="215px">
+                    </article>
+                   
+                </article>
+            </section>
+            <section class="products">
+                <article class="product">
+                    <div class="product-image">
+                    </div>
+                    <h1>Table</h1>
+                    <p>$20</p>
+                </article>
+                <article class="product">
+                        <div class="product-image">
+                        </div>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+                <article class="product">
+                        <div class="product-image">
+                        </div>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+
+            </section>
+            <div class="more-products">
+                <button>View More</button>
+            </div>
+        </main>
+    </body>
+    
+   
+
+</template>
