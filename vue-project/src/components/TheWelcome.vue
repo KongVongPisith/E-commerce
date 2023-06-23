714db2a69a6043519a2b57ee54049c646ba2eee1
Kong Vongpisith <115731062+KongVongPisith@users.noreply.github.com>
2023-06-23 21:51:43 +0700

    vue-project

---
+++ vue-project/src/components/TheWelcome.vue
@@ -0,0 +1,157 @@
+<template>
+    <body>
+        <header class="header">
+            <h1 class="h1">Blazer-Eco</h1>
+        </header>
+
+        <main class="main">
+            <section class="wrapper-top">
+                <article class="top">
+                    <article>
+                        <h2>Buy & Sell Accessories in one click</h2>
+                        <form action="">
+                            <input name="search" class="search" type="search" placeholder="Search">
+                            
+                        </form>
+                    </article>
+                    <article>
+                         <img src="../assets/img/pic1 (6).png" alt="" width="471px" height="343px"/>
+                    </article>
+                   
+                </article>
+            </section>
+
+            <section class="serve">
+                <article class="serve1">
+                    <article class="usedandnew">
+                        <h3>Used & New</h3>
+                        <p>We sell any accessories for daily life and working</p>
+                        <button>View More</button>
+                    </article>
+                    <article>
+                        <img src="../assets/img/pic1 (1).png" alt="" width="214px" height="215px">
+                    </article>
+                </article>
+
+                <article class="serve2">
+                    <article class="service">
+                        <h3>Services</h3>
+                        <p>Warranty for 6 month and discount for fixing otherwise if our product is disable you can contact us and we will refund money back</p>
+                        <button>View More</button>
+                    </article>
+                    <article>
+                        <img src="../assets/img/pic1 (7).png" alt="" width="214px" height="215px">
+                    </article>
+                   
+                </article>
+            </section>
+            <section class="products">
+                <article class="product">
+                    <article class="product-image">
+                    </article>
+                    <h1>Table</h1>
+                    <p>$20</p>
+                </article>
+                <article class="product">
+                        <article class="product-image">
+                        </article>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+                <article class="product">
+                        <article class="product-image">
+                        </article>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+                <article class="product">
+                    <article class="product-image">
+                    </article>
+                    <h1>Table</h1>
+                    <p>$20</p>
+                </article>
+                <article class="product">
+                        <article class="product-image">
+                        </article>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+                <article class="product">
+                        <article class="product-image">
+                        </article>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+                <article class="product">
+                    <article class="product-image">
+                    </article>
+                    <h1>Table</h1>
+                    <p>$20</p>
+                </article>
+                <article class="product">
+                        <article class="product-image">
+                        </article>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+                <article class="product">
+                        <article class="product-image">
+                        </article>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+                <article class="product">
+                        <article class="product-image">
+                        </article>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+                <article class="product">
+                        <article class="product-image">
+                        </article>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+                <article class="product">
+                    <article class="product-image">
+                    </article>
+                    <h1>Table</h1>
+                    <p>$20</p>
+                </article>
+                <article class="product">
+                        <article class="product-image">
+                        </article>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+                <article class="product">
+                        <article class="product-image">
+                        </article>
+                        <h1>Table</h1>
+                        <p>$20</p>
+                </article>
+
+            </section>
+            <!-- <article class="more-products">
+                <button>View More</button>
+            </article> -->
+            
+        </main>
+        <footer class="footer">
+            <section class="online-shop">
+                <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook' }"/>
+            </section>
+            <section class="contact">
+                <article>
+                    
+                </article>
+            </section>
+            <section class="about">
+
+            </section>
+        </footer>
+    </body>
+    
+   
+
+</template>
